29fed96a3b838100996edb472cfd7cf7372faf58
Eyre_S <sukazyo@outlook.com>
2022-09-25 21:59:45 +0800

    完成了 morny java 所使用的 messiva 日志记录器的简单重构的 Log 模型和控制台输出格式化工具

---
+++ logger/log_appender.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+import threading
+from .log import *
+
+def format_message (log:Log) -> str :
+	
+	origins = log.message.split("\n")
+	
+	message = ""
+	message += f"[{datetime.fromtimestamp(log.timestamp/1000/1000/1000).strftime('%Y-%m-%d/%H:%M:%S:%f')}]" + \
+		f"[{threading.current_thread().name }]"
+	promptNewLine = "'"*len(message)
+	message += f"[{log.level.name}]{origins[0]}"
+	for i in range(len(origins)-1) :
+		message += f"\n{promptNewLine}[{log.level.name}]{origins[i+1]}"
+	
+	return message
+	
